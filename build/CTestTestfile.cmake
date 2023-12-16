d66214ae55380ffedc1513070e3d2a043b16a55b
zfshenGit <zhifei_shen@163.com>
2023-12-16 12:39:56 +0800

    zfshen,修改

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shenzhifei/my/code/urdf_ws/src
-# Build directory: /home/shenzhifei/my/code/urdf_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ur_description")
-subdirs("robotiq_description")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/shenzhifei/my/code/urdf_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/shenzhifei/my/code/urdf_ws/devel;/opt/ros/noetic'.split(';') if '/home/shenzhifei/my/code/urdf_ws/devel;/opt/ros/noetic' != '' else []
